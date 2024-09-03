ff08bfbcc59b52f4179080df44fc1431a5d6a95e
Renzo Barrios <renzo.barrios1407@gmail.com>
2024-09-02 23:30:23 -0500

    [update] added module 11 more html example

--- Module_11/84_simple_html.py
+++ Module_11/84_simple_html.py
@@ -16,3 +16,33 @@ SIMPLE_HTML = '''<html>
 </ul>
 </body>
 </html>'''
+
+simple_soup = beautifulSoup(SIMPLE_HTML, 'html.parser') # HTML parser is all the document
+
+print(simple_soup.find('h1'))
+print(simple_soup.find('h1').string)
+
+
+def find_title():
+     h1_tag = simple_soup.find(h1)
+     print(h1_tag.string)
+
+def find_list_items():
+     list_items = simple_soup.find_all('li')
+     list_contents = [e.string for w in list_items]
+     print(list_items)     
+
+def find_subtitle():
+     paragraph = simple_soup.find('p', {'class': 'subtitle'})
+     print(paragraph.string)
+
+def find_other_paragraph():
+     paragraphs = simple_soup.find_all('p')
+     other_paragraphs = [p for p in paragraphs if 'subtle' not in p.atrrs.get('class', [] )]
+     print(other_paragraphs[0].string)
+
+
+
+
+find_title()
+find_list_items()
