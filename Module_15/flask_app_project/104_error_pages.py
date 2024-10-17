c5b4e9706be8589dc69313db8167ae28e5bd1854
Renzo Barrios <renzo.barrios1407@gmail.com>
2024-10-17 00:02:36 -0500

    [update] added module 15 Flask all completed lectures

---
+++ Module_15/flask_app_project/104_error_pages.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+posts = {
+    0: {
+        'title': 'Hello, world',
+        'content': 'This is my first blog post!'
+    }
+}
+
+
+@app.route('/')
+def home():
+    return 'Hello, world!'
+
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = posts.get(post_id)
+    # If the post was not found, then we can render a different page instead—the user will see an error page.
+    if not post:
+        return render_template('404.jinja2', message=f'A post with id {post_id} was not found.')
+    return render_template('post.jinja2', post=post)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
