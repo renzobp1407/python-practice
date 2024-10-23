e8e31b2c9eb63235efd44845ad9ddadd568f0666
Renzo Barrios <renzo.barrios1407@gmail.com>
2024-10-23 00:01:32 -0500

    [update] added module 18 second part on setters and interfaces in python 2

--- Module_18/property_setters/app.py
+++ Module_18/property_setters/app.py
@@ -1,14 +1,14 @@
-from flight import Segment, Flight
-
+from flight import Segment, flight
 
 seg = [Segment('EDI', 'LHR'), Segment('LHR', 'CAN')]
-flight = Flight(seg)
+flight = flight(seg)
 
 print(flight.departure_point)
 print(flight)
 
 flight.departure_point = 'GLA'
-print('...Set departure to GLA...')
+print('... Set departure to GLA...')
 
 print(flight.departure_point)
 print(flight)
+
