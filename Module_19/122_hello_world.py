a88980c6d3c29be8508490de37bff724f805d68c
Renzo Barrios <renzo.barrios1407@gmail.com>
2024-10-25 22:32:25 -0500

    [update] added first tkinter label and frames explanation

--- Module_19/122_hello_world.py
+++ Module_19/122_hello_world.py
@@ -1,10 +1,8 @@
 import tkinter as tk
 from tkinter import ttk
 
-
 def greet():
-    print("Hello, World!")
-
+    print("Hello, world")
 
 root = tk.Tk()
 root.title("Hello")
@@ -13,6 +11,7 @@ greet_button = ttk.Button(root, text="Greet", command=greet)
 greet_button.pack(side="left", fill="x", expand=True)
 
 quit_button = ttk.Button(root, text="Quit", command=root.destroy)
-quit_button.pack(side="left", fill="x", expand=True)  # could use side="right"
+quit_button.pack(side="left", fill="x", expand=True)
+
 
 root.mainloop()
